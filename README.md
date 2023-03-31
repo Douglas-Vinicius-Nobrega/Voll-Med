9a048a9a6071f54575636521c979010e1b5b5fd0
dougrete2014@outlook.com <dougrete2014@outlook.com>
2023-03-31 18:01:13 -0300

    Requisições PUT e Delete lógico

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Voll-Med
 Clínica médica fictícia
+
+# Sobre o Projeto e Tecnologias Utilizadas
+
+Criei um API REST utilizando Spring Boot e também desenvolvi um CRUD.
+
+Usei o Spring Initializr para fazer a construção do projeto.
+
+Utilizei bibliotecas, como o driver do MySQL, o Flyway e o Lombok.
+
+Fiz algumas migrations para ter controle do histórico de evolução do banco de dados e a implementar a API Rest a partir da criação dos controllers. 
+
+utilizei repositories, para simplificar o acesso ao banco de dados e ao JPA.
+
+Fiz o mapeamento das entidades JPA. Implementamos, também, o CRUD com validações.
+
--- api/src/main/java/com/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/com/med/voll/api/controller/MedicoController.java
@@ -4,13 +4,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import java.util.List;
 
+import com.med.voll.api.medico.DadosAtualizacaoMedico;
 import com.med.voll.api.medico.DadosCadastroMedico;
 import com.med.voll.api.medico.DadosListagemMedico;
 import com.med.voll.api.medico.Medico;
@@ -27,28 +30,31 @@ public class MedicoController {
 	@Autowired
 	private MedicoRepository medicoRepository;
 
-	@PostMapping // requisição do tipo POST
-	@Transactional // metodo de salvar, atualizar ou excluir algo do db
+	@PostMapping
+	@Transactional 
 	public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados) {
 		medicoRepository.save(new Medico(dados));
 	}
 	
-//	@GetMapping // devolver uma informação
-//	// além de devolver a lista de dados, o Page vai devolver dados da paginação
-//	// pois quenrendo listar a cada 10 médicos por vez e em ordem alfabética
-//	public List<DadosListagemMedico> listar(Pageable paginacao) {
-//		// convertendo de uma lista de médicos, para uma lista de DadosListagemMedico
-//		// Pois estamos trazendo algumas informações de medicos e não todas.
-//		return medicoRepository.findAll().stream().map(DadosListagemMedico::new).toList();
-//	}
-	
 	@GetMapping // devolver uma informação
-	// além de devolver a lista de dados, o Page vai devolver dados da paginação
-	// pois quenrendo listar a cada 10 médicos por vez e em ordem alfabética
 	public Page<DadosListagemMedico> listar(@PageableDefault(size = 10, page = 0, sort = {"nome"}) Pageable paginacao) {
-		// convertendo de uma lista de médicos, para uma lista de DadosListagemMedico
-		// Pois estamos trazendo algumas informações de medicos e não todas.
-		return medicoRepository.findAll(paginacao).map(DadosListagemMedico::new);
+		
+		return medicoRepository.findAllByAtivoTrue(paginacao).map(DadosListagemMedico::new);
+	}
+	
+	@PutMapping
+	@Transactional // vai fazer uma atualização no db
+	public void atualizar(@RequestBody @Valid DadosAtualizacaoMedico dados) {
+		var medico = medicoRepository.getReferenceById(dados.id());
+		medico.atualizarInformacoes(dados);		
+	}
+	
+	// exclusão lógico
+	@DeleteMapping("/{id}")
+	@Transactional 
+	public void excluir(@PathVariable Long id) {
+		var medico = medicoRepository.getReferenceById(id);
+		medico.excluir();
 	}
 	
 }
